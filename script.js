52a1ba72a33427922d62b4ee2baf7ce7324a233e
sanjida khan <khanatia26@gmail.com>
2018-05-21 04:42:34 +0000

    jscript

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 /* JavaScript code here */
+//Task1
+console.log("External JS")
+
+//Task2
+function sumArray(arr){
+    var sum = 0;
+    arr.foreach(function(value, index){
+        sum += value;
+        
+    });
+    return sum;
+    
+}
+
+console.log(sumArray([1,2,3,4,5]));
